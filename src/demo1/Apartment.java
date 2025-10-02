cb29a1d1b6896df320206899b46d0395552bbe65
spaner <hunggwralt@gmail.com>
2025-10-02 14:44:01 -0700

    added stuff

--- src/demo1/Apartment.java
+++ src/demo1/Apartment.java
@@ -6,12 +6,7 @@ public class Apartment extends House {
         super("concrete", 2);
     }
    
-    public static void main(String[] args) {
-        House h = new House();
-        h.getClean();
-        Apartment a = new Apartment();
-        a.getClean();
-    }
+
 }
 
 
